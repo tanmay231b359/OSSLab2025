34c045486390d713c9ddc458d7ec19dc3259e74d
231B359 <231B359@juet.co.in>
2025-07-29 15:12:01 +0530

    Added solution to Lab Exercise 1

--- LE1/GTN.java
+++ LE1/GTN.java
@@ -1,9 +1,13 @@
-public static void main(String args[])
-{
-	int a=5;
-	int b=6;
+public class GTN{
 
-	if(a>b)System.out.println(a);
 
-	else System.out.println(b);
+	public static void main(String args[])
+	{
+		int a=5;
+		int b=6;
+
+		if(a>b)System.out.println(a);
+
+		else System.out.println(b);
+	}
 }
